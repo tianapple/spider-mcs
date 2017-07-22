57f06a9cef5a5f1c9ea426ec5513349722c3b2f4
xckxue <xckxue@126.com>
2017-07-22 16:10:34 +0800

    增加js具类

--- mcs-common/src/main/resources/static/js/common.js
+++ mcs-common/src/main/resources/static/js/common.js
@@ -1,3 +1,24 @@
+/**
+ * 使panel和datagrid在加载时提示
+ * @requires jQuery,EasyUI
+ */
+$.fn.panel.defaults.loadingMessage = '加载中....';
+$.fn.datagrid.defaults.loadMsg = '加载中....';
+
+/**
+ * @requires jQuery
+ * 去掉空格
+ * **/
+String.prototype.trim = function() {
+    return this.replace(/(^\s*)|(\s*$)/g, '');
+};
+String.prototype.ltrim = function() {
+    return this.replace(/(^\s*)/g, '');
+};
+String.prototype.rtrim = function() {
+    return this.replace(/(\s*$)/g, '');
+};
+
 //将表单数据转为json
 $.fn.serializeObject = function () {
     var o = {};
@@ -17,7 +38,6 @@ $.fn.serializeObject = function () {
 
 //全局配置ajax
 $.ajaxSetup({
-    dataType: "json",
     type: 'POST',
     complete: function (XMLHttpRequest, textStatus) {
         if (XMLHttpRequest.getResponseHeader("sessionstatus") == 'timeout') {
@@ -41,7 +61,132 @@ function getTopWinow(){
     return p;
 }
 
-//百分比宽度
-function fixWidth(percent) {
-    return document.body.clientWidth * percent;//根据自身情况更改
-}
+/**
+ * @requires jQuery
+ * 防止退格键导致页面回退
+ */
+$(document).keydown(function (e) {
+    var doPrevent;
+    if (e.keyCode == 8) {
+        var d = e.srcElement || e.target;
+        if (d.tagName.toUpperCase() == 'INPUT' || d.tagName.toUpperCase() == 'TEXTAREA') {
+            doPrevent = d.readOnly || d.disabled;
+        }else{
+            doPrevent = true;
+        }
+    }else{
+        doPrevent = false;
+    }
+    if (doPrevent)
+        e.preventDefault();
+});
+
+
+
+/**
+ *
+ * @requires jQuery,EasyUI
+ *
+ * 创建一个模式化的dialog
+ *
+ * @returns $.modalDialog.handler 这个handler代表弹出的dialog句柄
+ *
+ * @returns $.modalDialog.xxx 这个xxx是可以自己定义名称，主要用在弹窗关闭时，刷新某些对象的操作，可以将xxx这个对象预定义好
+ */
+$.modalDialog = function(options) {
+    if ($.modalDialog.handler == undefined) {// 避免重复弹出
+        var opts = $.extend({
+            title : '',
+            width : 840,
+            height : 680,
+            modal : true,
+            onClose : function() {
+                $.modalDialog.handler = undefined;
+                $(this).dialog('destroy');
+            },
+            onOpen : function() {
+            }
+        }, options);
+        opts.modal = true;// 强制此dialog为模式化，无视传递过来的modal参数
+        return $.modalDialog.handler = $('<div/>').dialog(opts);
+    }
+};
+
+/**
+ * @requires jQuery,EasyUI
+ *
+ * 防止panel/window/dialog组件超出浏览器边界
+ * @param left
+ * @param top
+ */
+var easyuiPanelOnMove = function(left, top) {
+    var l = left;
+    var t = top;
+    if (l < 1) {
+        l = 1;
+    }
+    if (t < 1) {
+        t = 1;
+    }
+    var width = parseInt($(this).parent().css('width')) + 14;
+    var height = parseInt($(this).parent().css('height')) + 14;
+    var right = l + width;
+    var buttom = t + height;
+    var browserWidth = $(window).width();
+    var browserHeight = $(window).height();
+    if (right > browserWidth) {
+        l = browserWidth - width;
+    }
+    if (buttom > browserHeight) {
+        t = browserHeight - height;
+    }
+    $(this).parent().css({/* 修正面板位置 */
+        left : l,
+        top : t
+    });
+};
+$.fn.dialog.defaults.onMove = easyuiPanelOnMove;
+$.fn.window.defaults.onMove = easyuiPanelOnMove;
+$.fn.panel.defaults.onMove = easyuiPanelOnMove;
+
+/**
+ * @requires jQuery,EasyUI
+ *
+ * panel关闭时回收内存，主要用于layout使用iframe嵌入网页时的内存泄漏问题
+ */
+$.fn.panel.defaults.onBeforeDestroy = function() {
+    var frame = $('iframe', this);
+    try {
+        if (frame.length > 0) {
+            for ( var i = 0; i < frame.length; i++) {
+                frame[i].src = '';
+                frame[i].contentWindow.document.write('');
+                frame[i].contentWindow.close();
+            }
+            frame.remove();
+            if (navigator.userAgent.indexOf("MSIE") > 0) {// IE特有回收内存方法
+                try {
+                    CollectGarbage();
+                } catch (e) {
+                }
+            }
+        }
+    } catch (e) {
+    }
+};
+
+
+
+/**
+ * @requires jQuery,EasyUI
+ *
+ * 扩展validatebox，添加验证两次密码功能
+ */
+$.extend($.fn.validatebox.defaults.rules, {
+    eqPwd : {
+        validator : function(value, param) {
+            return value == $(param[0]).val();
+        },
+        message : '密码不一致！'
+    }
+});
