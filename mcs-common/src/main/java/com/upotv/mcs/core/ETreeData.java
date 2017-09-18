05d6f4518bfb25600f611414d65ae8c878e1dc44
xckxue <xckxue@126.com>
2017-09-18 11:40:31 +0800

    增加个treegrid公共

---
+++ mcs-common/src/main/java/com/upotv/mcs/core/ETreeData.java
@@ -0,0 +1,67 @@
+package com.upotv.mcs.core;
+
+import com.upotv.mcs.util.DateUtil;
+import org.apache.poi.ss.formula.functions.T;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by wow on 2017/8/21.
+ */
+public class ETreeData {
+    private Integer id;
+    private String text;
+    private String iconCls;
+    private String state = "closed";
+    private ETreeAttribute attributes;
+    private List<ETreeData> children;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getIconCls() {
+        return iconCls;
+    }
+
+    public void setIconCls(String iconCls) {
+        this.iconCls = iconCls;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public ETreeAttribute getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(ETreeAttribute attributes) {
+        this.attributes = attributes;
+    }
+
+    public List<ETreeData> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<ETreeData> children) {
+        this.children = children;
+    }
+}
