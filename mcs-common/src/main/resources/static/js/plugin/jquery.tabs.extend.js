4d5bb33e6a5d413daba20beffa3a033f5dbe092d
xckxue <xckxue@126.com>
2017-11-28 13:57:57 +0800

    增加dnd

--- mcs-common/src/main/resources/static/js/plugin/jquery.tabs.extend.js
+++ mcs-common/src/main/resources/static/js/plugin/jquery.tabs.extend.js
@@ -57,7 +57,7 @@ $.extend($.fn.tabs.methods, {
             $tabBody.css({'overflow':'hidden','position':'relative'});
             var $mask = $('<div style="position:absolute;z-index:2;width:100%;height:100%;background:#ccc;z-index:1000;opacity:0.3;filter:alpha(opacity=30);"><div>').appendTo($tabBody);
             //var $maskMessage = $('<div class="mask-message" style="z-index:3;width:auto;height:16px;line-height:16px;position:absolute;top:30%;left:50%;margin-top:-20px;margin-left:-92px;border:2px solid #95B8E7;padding: 12px 5px 10px 30px;background: #ffffff url(\'/images/loading.gif\') no-repeat scroll 5px center;">' + (params.iframe.message || 'Processing, please wait ...') + '</div>').appendTo($tabBody);
-            var cnt = Math.floor(Math.random()*5+1);
+            var cnt = Math.floor(Math.random()*10+1);
             var src ="/images/loading"+cnt+".gif";
             var $maskMessage = $('<div class="mask-message" style="z-index:3;position:absolute;top:20%;left:35%;"><img src="'+src+'"/></div>').appendTo($tabBody);
             var $containterMask = $('<div style="position:absolute;width:100%;height:100%;z-index:1;background:#fff;"></div>').appendTo($tabBody);
